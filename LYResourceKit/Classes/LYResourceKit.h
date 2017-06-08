2e37d22b8dda5ac394a5764d9df9de55d679f6dd
Luo Yu (MacBook Pro) <indie.luo@gmail.com>
2017-06-08 13:49:16 +0800

    [Add] main object

---
+++ LYResourceKit/Classes/LYResourceKit.h
@@ -0,0 +1,13 @@
+//
+//  LYResourceKit.h
+//  LYRESOURCEKIT
+//
+//  CREATED BY LUO YU ON 2017-06-08.
+//  COPYRIGHT (C) 2017 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LYResourceKit : NSObject
+
+@end
---
+++ LYResourceKit/Classes/LYResourceKit.m
@@ -0,0 +1,13 @@
+//
+//  LYResourceKit.m
+//  LYRESOURCEKIT
+//
+//  CREATED BY LUO YU ON 2017-06-08.
+//  COPYRIGHT (C) 2017 LUO YU. ALL RIGHTS RESERVED.
+//
+
+#import "LYResourceKit.h"
+
+@implementation LYResourceKit
+
+@end
